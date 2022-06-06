b65d75181d13c76c95bd6f6ef27409ff61d0299b
proton-2022 <smurffy-2022@proton.me>
2022-06-06 18:30:23 +0000

    pass local R CMD check.

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,14 +1,16 @@
-#' Read data from a csv file and constructs a tibble. 
+#' Read data from a csv file and constructs a tibble.
 #'
-#' @param filename A character specifying the csv file to read. A non-existing 
-#' filename will produce an error. 
-#' @return a tibble containing the csv data. 
+#' @param filename A character specifying the csv file to read. A non-existing
+#' filename will produce an error.
+#' @return a tibble containing the csv data.
 #' @importFrom readr read_csv
 #' @importFrom dplyr tbl_df
 #' @examples
+#' \dontrun{
 #' fn <- "data/accident_2013.csv.bz2"
 #' data <- fars_read(fn)
-#' # filename that does not exist gives an error. 
+#' }
+#' # filename that does not exist gives an error.
 #' \dontrun{
 #' fn <- NULL
 #' data <- fars_read(fn)
@@ -23,16 +25,16 @@ fars_read <- function(filename) {
         dplyr::tbl_df(data)
 }
 
-#' Generates a filename for a specific year. 
-#' 
-#' @param year A character or numeric specifying a year. Input \code{year} that 
-#' cannot be casted to an integer will result in an error or an empty return array. 
-#' @return A string in the format "accidient_%d.csv.bz2". 
-#' @examples 
-#' fn <- make_filename(year="2020")  
+#' Generates a filename for a specific year.
+#'
+#' @param year A character or numeric specifying a year. Input \code{year} that
+#' cannot be casted to an integer will result in an error or an empty return array.
+#' @return A string in the format "accidient_%d.csv.bz2".
+#' @examples
+#' fn <- make_filename(year="2020")
 #' fn <- make_filename(year=2020)
-#' # input values that cannot be casted to an integer will result in an error or 
-#' # empty return array 
+#' # input values that cannot be casted to an integer will result in an error or
+#' # empty return array
 #' \dontrun{
 #' fn <- make_filename(year=NULL)
 #' fn <- make_filename(year="hello")
@@ -43,22 +45,23 @@ make_filename <- function(year) {
         sprintf("accident_%d.csv.bz2", year)
 }
 
-#' Returns occurrence of accidents in a specific year. 
-#' 
-#' @param years A vector (atomic or list) of or a single numeric or character 
-#' specifying years of accidents to return. Giving \code{years} that cannot 
-#' be casted to a numeric, or for which no data exist will return NULL and 
-#' a warning message. 
-#' @return A tibble with 'MONTH' and 'year' columns specifying the month of 
-#' accidents in the year. 
-#' @importFrom dplyr mutate select 
+#' Returns occurrence of accidents in a specific year.
+#'
+#' @param years A vector (atomic or list) of or a single numeric or character
+#' specifying years of accidents to return. Giving \code{years} that cannot
+#' be casted to a numeric, or for which no data exist will return NULL and
+#' a warning message.
+#' @return A tibble with 'MONTH' and 'year' columns specifying the month of
+#' accidents in the year.
+#' @importFrom dplyr mutate select
 #' @importFrom magrittr %>%
 #' @examples
+#' \dontrun{
 #' dat <- fars_read_years(2013)
 #' dat <- fars_read_year(c(2013, 2014))
 #' dat <- fars_read_year("2013")
-#' \dontrun{
-#' dat <- fars_read_year(c(2013, "hello")) # returns NULL for "hello"
+#' # returns NULL for invalid inputs:
+#' dat <- fars_read_year(c(2013, "hello"))
 #' }
 #' @export
 fars_read_years <- function(years) {
@@ -66,7 +69,7 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -75,56 +78,57 @@ fars_read_years <- function(years) {
         })
 }
 
-#' Summarizes the number of accidents in each month for specific years. 
-#' 
-#' @param years A vector (atomic or list) of or a single numeric or character 
-#' specifying years of accidents to return. \code{years} input for which no 
-#' data exist or that cannot be casted to a year will be skipped. An error 
+#' Summarizes the number of accidents in each month for specific years.
+#'
+#' @param years A vector (atomic or list) of or a single numeric or character
+#' specifying years of accidents to return. \code{years} input for which no
+#' data exist or that cannot be casted to a year will be skipped. An error
 #' will occur when no valid inputs are provided.
-#' @return A tibble that summarizes that number of accidents in the years 
-#' for which data exist. 
+#' @return A tibble that summarizes that number of accidents in the years
+#' for which data exist.
 #' @importFrom dplyr bind_rows group_by summarize n
 #' @importFrom tidyr spread
 #' @importFrom magrittr %>%
-#' @examples 
+#' @examples
+#' \dontrun{
 #' summary_table <- fars_summarize_years(2013)
 #' summary_table <- fars_summarize_years(c(2013, 2014))
 #' summary_table <- fars_summarize_years("2013")
-#' 
+#'
 #' # no column will be returned for invalid inputs
 #' summary_table <- fars_summarize_years(c(2013, "hello"))
-#' \dontrun{
-#' summary_table <- fars_summarize_years("hello") # will result in an error
+#' # error for single invalid inputs
+#' summary_table <- fars_summarize_years("hello")
 #' }
 #' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
 
-#' Plots the locations of accidents for a specific state in a year. 
-#' 
+#' Plots the locations of accidents for a specific state in a year.
+#'
 #' @param state.num A value that indicates a state ID. Must be able to be casted
-#' to an integer between 1 and 56. Error will occur if \code{year} is not a single, 
-#' valid value or there is no existing data for that year; or if \code{state.num} 
+#' to an integer between 1 and 56. Error will occur if \code{year} is not a single,
+#' valid value or there is no existing data for that year; or if \code{state.num}
 #' is not a valid state number.
-#' @param year A value that specifies a year. Must be able to be casted to an 
-#' integer that specifies a valid year. 
-#' @return None. Draws a map a of a state and points indicating locations of 
-#' accidents, if there are accidents to plot. Otherwise draw nothing. 
-#' @importFrom dplyr filter 
-#' @importFrom maps map 
+#' @param year A value that specifies a year. Must be able to be casted to an
+#' integer that specifies a valid year.
+#' @return None. Draws a map a of a state and points indicating locations of
+#' accidents, if there are accidents to plot. Otherwise draw nothing.
+#' @importFrom dplyr filter
+#' @importFrom maps map
 #' @importFrom graphics points
 #' @examples
+#' \dontrun{
 #' fars_map_state(1, 2013)
 #' fars_map_state(19, 2014)
-#' \dontrun{
-#' # error will be produced: 
+#' # error will be produced:
 #' fars_map_state(100, 2013) # invalid state.num
-#' fars_map_state(1, 2017) # invalid year 
+#' fars_map_state(1, 2017) # invalid year
 #' }
 #' @export
 fars_map_state <- function(state.num, year) {
